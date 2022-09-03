1db28d2ea11f253605200299035c4f1839f58e79
mdammar786 <98681797+mdammar786@users.noreply.github.com>
2022-09-03 15:52:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require ('express')
+const app = express()
+const port = process.env.PORT || 5000
+const path = require('path')
+
+
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('/api', (req,res) => {
+    res.json({
+        message: 'This is the api endpoint'
+    })
+})
+
+app.get('/*', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+
+app.listen(port, _ => {
+    console.log(`Server is running on port ${port}`)
+})
